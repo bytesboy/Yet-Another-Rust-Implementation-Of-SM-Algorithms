1a624ec11a50f209284a8b58f8c722c6fb617238
尼维森莫·纳默帝敖 <icocoabeans@gmail.com>
2021-12-02 22:04:29 +0800

    refactor sm3 struct

--- src/sm3.rs
+++ src/sm3.rs
@@ -1,7 +1,8 @@
 mod core;
 
+/// 计算摘要信息
 pub fn digest(data: &str) -> String {
-    core::Crypto::new(data.as_bytes()).pad().block().iterate().hash()
+    core::Crypto::init(data.as_bytes()).pad().block().iterate().hash()
 }
 
 
