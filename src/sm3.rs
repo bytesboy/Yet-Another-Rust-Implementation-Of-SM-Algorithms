7bb3c74f2aaa77d71a00e0db75f91f32c3c08a12
尼维森莫·纳默帝敖 <icocoabeans@gmail.com>
2021-12-08 17:58:46 +0800

    sm3 new

--- src/sm3.rs
+++ src/sm3.rs
@@ -2,7 +2,7 @@ mod core;
 
 /// 计算摘要信息
 pub fn digest(data: &str) -> String {
-    core::Crypto::init(data.as_bytes()).pad().block().iterate().hash()
+    core::Crypto::new(data.as_bytes()).pad().block().iterate().hash()
 }
 
 
