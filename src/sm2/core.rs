bebc996fa54df2cc8e04c609e7c36852df7e2a62
尼维森莫·纳默帝敖 <icocoabeans@gmail.com>
2022-01-25 11:38:08 +0800

    sm2 key core

--- src/sm2/core.rs
+++ src/sm2/core.rs
@@ -1,111 +1,27 @@
-use std::cmp::Ordering;
-use std::mem;
-use std::sync::Once;
-
 use num_bigint::BigUint;
-use num_integer::Integer;
-use num_traits::Num;
+
 
 pub trait EllipticProvider {
-    fn initialize() -> Self;
+    fn blueprint(&self) -> &Elliptic;
+    /// 标量乘法
+    fn scalar_multiply(&self, x: BigUint, y: BigUint, k: BigUint) -> (BigUint, BigUint);
+    /// 基点标量乘法
+    fn scalar_base_multiply(&self, k: BigUint) -> (BigUint, BigUint);
 }
 
-pub trait Multiplication {
-    fn multiply(&self, scalar: BigUint) -> Point;
-}
-
-
+/// 使用SM2椭圆曲线公钥密码算法推荐曲线参数
+///
+/// y^2 = x^3 + ax + b
 #[derive(Clone, Debug)]
 pub struct Elliptic {
-    // the order of the finite field
-    p: BigUint,
-    // the constant a and b of the curve equation: y^2 = x^3 + ax +b
-    a: BigUint,
-    b: BigUint,
-    // the base point g(gx, gy) and order n
-    g: BasePoint,
-    bits: usize,
+    pub p: BigUint,
+    pub a: BigUint,
+    pub b: BigUint,
+    pub gx: BigUint,
+    pub gy: BigUint,
+    pub n: BigUint,
+    pub bits: usize,
 }
-
-
-impl Elliptic {
-    pub fn new(p: BigUint, a: BigUint, b: BigUint, g: BasePoint, bits: usize) -> Self {
-        Elliptic { p, a, b, g, bits }
-    }
-
-    pub fn base(&self) -> &BasePoint {
-        &self.g
-    }
-
-    pub fn bits(&self) -> usize {
-        self.bits
-    }
-
-    pub fn p(&self) -> &BigUint {
-        &self.p
-    }
-}
-
-
-#[derive(Clone, Debug)]
-pub struct Point(BigUint, BigUint);
-
-impl Point {
-    pub fn new(x: BigUint, y: BigUint) -> Self { Point(x, y) }
-}
-
-impl Multiplication for Point {
-    fn multiply(&self, scalar: BigUint) -> Point {
-        self.clone()
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct BasePoint {
-    point: Point,
-    order: BigUint,
-}
-
-
-impl BasePoint {
-    pub fn new(point: Point, order: BigUint) -> Self { BasePoint { point, order } }
-
-    pub fn point(&self) -> &Point {
-        &self.point
-    }
-
-    pub fn order(&self) -> &BigUint {
-        &self.order
-    }
-}
-
-
-impl Multiplication for BasePoint {
-    fn multiply(&self, scalar: BigUint) -> Point {
-        let scalar = {
-            // compare scalar and order, n = (scalar mod order) if scalar > order else scalar
-            if let Ordering::Greater = scalar.cmp(&self.order) {
-                scalar.mod_floor(&self.order)
-            } else {
-                scalar
-            }
-        };
-
-
-        self.point.clone()
-    }
-}
-
-/// 椭圆曲线上的点集记为E(Fp) = {(x,y,z)|x,y,z ∈ Fp且满足曲线方程y^2 = x^3 + axz^4 + bz^6}，
-/// 其中a,b ∈ Fp， 且4a^3 +27b^2 != 0 mod p。
-pub struct JacobPoint(BigUint, BigUint, BigUint);
-
-impl JacobPoint {
-    pub fn new(x: BigUint, y: BigUint, z: BigUint) -> Self {
-        JacobPoint(x, y, z)
-    }
-}
-
 
 
 
--- src/sm2/key.rs
+++ src/sm2/key.rs
@@ -1,82 +1,44 @@
 use std::ops::{Add, Sub};
-
 use num_bigint::BigUint;
 use num_integer::Integer;
 use num_traits::FromPrimitive;
-
-use crate::sm2::core::{BasePoint, Elliptic, Multiplication, Point};
-
+use crate::sm2::core::EllipticProvider;
 
 /// 公钥 非压缩：65bytes 压缩：33bytes
 #[derive(Clone, Debug)]
-struct PublicKey(Point);
-
-impl PublicKey {
-    fn new(key: Point) -> Self {
-        PublicKey(key)
-    }
-
-    fn from(private: &PrivateKey, base: &BasePoint) -> Self {
-        PublicKey(base.multiply(private.0.clone()))
-    }
-}
-
+pub struct PublicKey(BigUint, BigUint);
 
 /// 私钥 32bytes
 #[derive(Clone, Debug)]
-struct PrivateKey(BigUint);
-
-impl PrivateKey {
-    fn new(key: &BigUint) -> Self {
-        PrivateKey(key.clone())
-    }
-}
-
+pub struct PrivateKey(BigUint);
 
 /// 秘钥对（d, P）d:私钥 P:公钥
 #[derive(Debug)]
-struct KeyPair(PrivateKey, PublicKey);
+pub struct KeyPair(PrivateKey, PublicKey);
 
 impl KeyPair {
     pub fn private_key(&self) -> &PrivateKey {
         &self.0
     }
-
     pub fn public_key(&self) -> &PublicKey {
         &self.1
     }
 }
 
-
 /// 秘钥生成器
-///
-/// 使用SM2椭圆曲线公钥密码算法推荐曲线参数
-///
-/// 256位素数域
-///
-/// y^2 = x^3 + ax + b
-#[derive(Clone, Debug)]
 pub struct KeyGenerator {
-    elliptic: Elliptic,
+    elliptic: Box<dyn EllipticProvider>,
 }
 
 impl KeyGenerator {
-    pub fn new(elliptic: &Elliptic) -> Self {
-        KeyGenerator { elliptic: elliptic.clone() }
+    pub fn new(elliptic: Box<dyn EllipticProvider>) -> Self {
+        KeyGenerator { elliptic }
     }
 
-    /// 输入:一个有效的Fq(q = p且p为大于3的素数，或q = 2m)上椭圆曲线系统参数的集合。
-    ///
-    /// 输出:与椭圆曲线系统参数相关的一个密钥对(d , P)。
-    ///
-    /// a) 用随机数发生器产生整数d ∈ \[1, n − 2];
-    ///
-    /// b) G为基点，计算点P = (xP,yP) = \[d]G;
-    ///
-    /// c) 密钥对是(d,P)，其中d为私钥，P为公钥。
     pub fn gen_key_pair(&self) -> KeyPair {
         let private_key = self.gen_private_key();
-        KeyPair(private_key.clone(), self.gen_public_key(&private_key).clone())
+        let public_key = self.gen_public_key(&private_key);
+        KeyPair(private_key.clone(), public_key.clone())
     }
 
     /// 生成私钥
@@ -84,44 +46,37 @@ impl KeyGenerator {
     /// 随机数生成整数d ∈ \[1, n − 2], n为椭圆曲线循环子群的阶
     fn gen_private_key(&self) -> PrivateKey {
         // private key: 32 bytes
-        let n = self.elliptic.base().order();
-        let bytes: Vec<u8> = (0..self.elliptic.bits() / 8 + 8).map(|_| { rand::random::<u8>() }).collect();
+        let elliptic = self.elliptic.blueprint();
+        let n = &elliptic.n;
+        let bytes: Vec<u8> = (0..elliptic.bits / 8 + 8).map(|_| { rand::random::<u8>() }).collect();
         let mut k = BigUint::from_bytes_be(&bytes);
         // n-2
         let n = BigUint::sub((*n).clone(), BigUint::from_u64(2).unwrap());
         // k % n  ∈ [0, n-1]  => k % (n-2) + 1  ∈ [1, n-2]
         let key = k.mod_floor(&n).add(BigUint::from_u64(1).unwrap());
-        PrivateKey::new(&key)
+        PrivateKey(key)
     }
 
     /// 生成公钥
     ///
     /// P = (xP,yP) = \[d]G, G为基点，d为私钥
     fn gen_public_key(&self, private_key: &PrivateKey) -> PublicKey {
-        PublicKey::from(private_key, self.elliptic.base())
+        let key = self.elliptic.scalar_base_multiply(private_key.0.clone());
+        PublicKey(key.0, key.1)
     }
 }
 
 
 #[cfg(test)]
 mod tests {
-    use num_bigint::BigUint;
     use num_traits::Num;
-
-    use crate::sm2::core::{BasePoint, Elliptic, EllipticProvider, Point};
-    use crate::sm2::key::{KeyGenerator, PrivateKey, PublicKey};
     use crate::sm2::p256::P256Elliptic;
+    use super::*;
 
     #[test]
     fn main() {
-        let elliptic = P256Elliptic::initialize();
-        let pair = KeyGenerator::new(elliptic.elliptic()).gen_key_pair();
-        println!("prk = {:?}", pair.private_key());
-        println!("puk = {:?}", pair.public_key());
-
-
-        let elliptic = P256Elliptic::initialize();
-        let pair = KeyGenerator::new(elliptic.elliptic()).gen_key_pair();
+        let generator = KeyGenerator::new(Box::new(P256Elliptic::init()));
+        let pair = generator.gen_key_pair();
         println!("prk = {:?}", pair.private_key());
         println!("puk = {:?}", pair.public_key());
     }
@@ -132,12 +87,12 @@ mod tests {
         // x: 76298453107918256108319614943154283626396976993715724710320433578462434588530
         // y: 22016840577845663905050918262284081863871275223913804750000840645022838962798
 
-        let elliptic = P256Elliptic::initialize();
         let prk = "48358803002808206747871163666773640956067045543241775523137833706911222329998";
         let prk = BigUint::from_str_radix(prk, 10).unwrap();
 
         let private_key = PrivateKey(prk);
-        let public_key = PublicKey::from(&private_key, elliptic.elliptic().base());
+        let generator = KeyGenerator::new(Box::new(P256Elliptic::init()));
+        let public_key = generator.gen_public_key(&private_key);
 
         println!("PriKey = {:?}", private_key);
         println!("PubKey = {:?}", public_key);
